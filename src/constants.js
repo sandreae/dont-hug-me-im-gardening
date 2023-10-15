0a5c954687947b2b7d0206698b12683d4846ef8d
Sam Andreae <contact@samandreae.com>
2023-10-16 00:34:27 +0100

    Update schema ids

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,10 @@
 // Schema this application uses
 export const GARDEN_SCHEMA_ID =
-  'garden_0020286e0fe57400f7974eb5c12300c5eb3bc9f5b633c0a0764209f42e4c6a7d7815';
+  'garden_00200a4d753b6cc6c0e8de7b3bcac1cff774a1448cc098b93c9defa0e4043bc80250';
 export const PLANT_SCHEMA_ID =
-  'plant_0020d5d98552977c63c95ac02f9ac0e123bde514c4e2be69a2a6cc18fa47b773a2fb';
+  'plant_0020f47272fb8fff361df611a6e0ca5355084739bad3c4ef959a8f7fc4d0db53ab5f';
 export const SPECIES_SCHEMA_ID =
-  'species_0020cd82879cc1a03ee500f07d2ad23214dced368531e0c9861cbda203718c05b159';
+  'species_0020bbe934cff0c2b552885f6f718f934998487d4091078c23a2c137cc065be93527';
 
 // URL of your local aquadoggo node
 export const ENDPOINT = 'http://localhost:2020/graphql';
