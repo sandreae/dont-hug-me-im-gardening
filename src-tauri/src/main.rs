c026299fa4042b7630b099ec2c2580be03a9d41e
Sam Andreae <contact@samandreae.com>
2023-11-16 23:22:34 +0000

    In dev mode run ephemeral node

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,19 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use std::fs::DirBuilder;
+use std::{fs::DirBuilder, path::PathBuf};
 
 mod bootstrap;
 mod key_pair;
 
 use aquadoggo::{Configuration, Node};
 use gql_client::Client;
-use tauri::api::path;
-use tauri::async_runtime;
+use p2panda_rs::identity::KeyPair;
+use tauri::{async_runtime, AppHandle};
 
 use crate::key_pair::generate_or_load_key_pair;
 
 const GRAPHQL_ENDPOINT: &str = "http://localhost:2020/graphql";
-const APP_DATA_DIR: &str = "p2p-garden";
 const BLOBS_DIR: &str = "blobs";
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -22,44 +21,61 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() {
-    async_runtime::spawn(async {
+#[cfg(not(dev))]
+fn get_app_data_dir(app_handle: &AppHandle) -> Option<PathBuf> {
+    app_handle.path_resolver().app_data_dir()
+}
+
+#[cfg(dev)]
+fn get_app_data_dir(app_handle: &AppHandle) -> Option<PathBuf> {
+    None
+}
+
+fn setup_handler(app: &mut tauri::App) -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let app_handle = app.handle();
+    let app_data_dir = get_app_data_dir(&app_handle).unwrap_or(std::path::PathBuf::new());
+
+    async_runtime::spawn(async move {
         // Enable logging if set via `RUST_LOG` environment variable.
         if std::env::var("RUST_LOG").is_ok() {
             let _ = env_logger::builder().try_init();
         }
 
-        // Get the recommended app data dir from tauri environment and append
-        // p2p-garden app dir to the path.
-        let data_dir = path::data_dir()
-            .expect("error deriving data dir path")
-            .join(APP_DATA_DIR);
-
         // Construct node configuration and set database url and blobs path.
         let mut config = Configuration::default();
-        config.database_url = format!(
-            "sqlite:{}/p2p-garden.sqlite3",
-            data_dir.to_str().expect("invalid character in path")
-        );
-        config.blobs_base_path = data_dir.join(BLOBS_DIR);
 
-        // Create blob the root app data directory and the blobs sub directory.
-        DirBuilder::new()
-            .recursive(true)
-            .create(data_dir.join(BLOBS_DIR))
-            .expect("error creating app data directories");
+        // If not in dev mode then configure node with tauri app data directory for persistence.
+        if !cfg!(dev) {
+            config.database_url = format!(
+                "sqlite:{}/p2p-garden.sqlite3",
+                app_data_dir.to_str().expect("invalid character in path")
+            );
+            config.blobs_base_path = app_data_dir.join(BLOBS_DIR);
+
+            // Create blobs sub directory.
+            DirBuilder::new()
+                .recursive(true)
+                .create(app_data_dir.join(BLOBS_DIR))
+                .expect("error creating app data directories");
+        }
 
         // Create a KeyPair or load it from secret.txt file in app data directory.
         //
         // This key pair is used to identify the node on the network, it is not used for signing
         // any application data.
-        let key_pair = generate_or_load_key_pair(data_dir.join("secret.txt"))
-            .expect("error generating or loading node key pair");
+        let key_pair = if cfg!(dev) {
+            // If we're in dev mode we always generate a temporary key pair.
+            KeyPair::new()
+        } else {
+            // Otherwise we try and load an existing one. 
+            generate_or_load_key_pair(app_data_dir.join("secret.txt"))
+                .expect("error generating or loading node key pair")
+        };
 
         // Start the node.
         let node = Node::start(key_pair, config).await;
 
-        // Load schema.lock file, by using the include macro, this file is included in compiled binaries.
+        // Load schema.lock file. By using the include macro, this file is included in compiled binaries.
         let data = include_str!("../schemas/schema.lock");
         let client = Client::new(&GRAPHQL_ENDPOINT);
 
@@ -69,7 +85,12 @@ fn main() {
         node.shutdown().await;
     });
 
+    Ok(())
+}
+
+fn main() {
     tauri::Builder::default()
+        .setup(setup_handler)
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
